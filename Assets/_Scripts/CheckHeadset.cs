d334908ffd901fb7f4d987fab8f9fc89c75d8c8a
Danivdwerf <danivdwerf.com>
2017-03-13 15:10:28 +0100

    check processes for steamvr

---
+++ Assets/_Scripts/CheckHeadset.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class CheckHeadset : MonoBehaviour 
+{
+	private void Awake()
+	{
+		foreach (Process pro in Process.GetProcesses ())
+		{
+			try
+			{
+				if(pro.ProcessName.ToLower().Contains("steamvr"))
+				{
+					print("Go for the vive");
+				}
+			}
+			catch{}
+		}
+	}
+}
