3c632dab23f13008e2122e5a56356c212b9c6e28
Danivdwerf <danivdwerf.com>
2017-03-21 22:34:48 +0100

    first bit of movement

---
+++ Assets/Acid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acid : MonoBehaviour 
+{
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.W))
+		{
+			this.transform.position += new Vector3(0,0,1);
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			this.transform.position += new Vector3(1,0,0);
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+			this.transform.position += new Vector3(0,0,-1);
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			this.transform.position += new Vector3(-1,0,0);
+		}
+	}
+}
---
+++ Assets/Heroin.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heroin : MonoBehaviour 
+{
+	private SteamVR_TrackedObject trackedObj;
+	private SteamVR_Controller.Device Controller{ get{return SteamVR_Controller.Input((int)trackedObj.index);}}
+	private GameObject collidingObject;
+	private GameObject objectInHand;
+
+	private void Awake()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+	}
+
+	private void Update()
+	{
+		if (Controller.GetTouch (Valve.VR.EVRButtonId.k_EButton_DPad_Up))
+		{
+			this.transform.position += new Vector3(0,0,1);
+		}
+
+		if (Controller.GetTouch (Valve.VR.EVRButtonId.k_EButton_DPad_Right))
+		{
+			this.transform.position += new Vector3(1,0,0);
+		}
+
+		if (Controller.GetTouch (Valve.VR.EVRButtonId.k_EButton_DPad_Down))
+		{
+			this.transform.position += new Vector3(0,0,-1);
+		}
+
+		if (Controller.GetTouch (Valve.VR.EVRButtonId.k_EButton_DPad_Left))
+		{
+			this.transform.position += new Vector3(-1,0,0);
+		}
+	}
+}
