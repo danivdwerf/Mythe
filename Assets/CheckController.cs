b376fc138802c8fb8a2d69deb9ab536c1b2304ca
Danivdwerf <danivdwerf.com>
2017-04-04 14:59:17 +0200

    startfiles for controller

---
+++ Assets/CheckController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckController : MonoBehaviour 
+{
+	private void Awake()
+	{
+		var joysticks = Input.GetJoystickNames();
+		for (int i = 0; i < joysticks.Length; i++)
+		{
+			if (joysticks [i].ToLower ().Contains ("sony"))
+				setPlaystation ();
+
+			if (joysticks [i].ToLower ().Contains ("xbox"))
+				setXbox ();
+		}
+	}
+
+	private void Update()
+	{
+		//if(Input.GetAxis("dpadUp"))
+		print(Input.GetAxis("dpadUp"));
+		//print (Input.GetAxis(Controller.leftStickX));
+	}
+
+	private void setPlaystation()
+	{
+		
+	}
+
+	private void setXbox()
+	{
+		
+	}
+}
---
+++ Assets/Controller.cs
@@ -0,0 +1,19 @@
+﻿public class Controller
+{
+	public const string triangle = "triangle";
+	public const string square = "square";
+	public const string cross = "cross";
+	public const string circle = "circle";
+	public const string leftStickX = "leftStickX";
+	public const string leftStickY = "leftStickY";
+	public const string rightStickX = "rightStickX";
+	public const string rightStickY = "rightStickY";
+	public const string l1 = "l1";
+	public const string r1 = "r1";
+	public const string l2 = "l2";
+	public const string r2 = "r2";
+	public const string leftThumb = "leftThumb";
+	public const string rightThumb = "rightThumb";
+	public const string dpadX = "dpadX";
+	public const string dpadY = "dpadY";
+}
